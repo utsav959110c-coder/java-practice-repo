c8c1921062067dc6ebdc2e48039ef2689918f993
utsav <utsav9591.10c@gmail.com>
2026-02-14 00:03:43 +0530

    change

---
+++ src/Arthematic_Operators.java
@@ -0,0 +1,14 @@
+public class Arthematic_Operators {
+    public static void main(String[] args){
+        int n1=2;
+        int n2=8;
+        int add=n1+n2;
+        int sub=n1-n2;
+        int mul=n1*n2;
+        float div=(float)n1/n2;
+        int rem=n1%n2;
+
+        System.out.println(div);
+
+    }
+}
---
+++ src/ResultCheck.java
@@ -0,0 +1,17 @@
+public class ResultCheck {
+    public static void main(String[] args) {
+        //write a program to check if student is pass or not
+        //inputs: name(string), marks(int),
+        //if marks >=40 ; pass else fail
+        String name="hari";
+        int marks=90;
+
+        if (marks>=40){
+
+            System.out.println(name + " is" + " PASS");
+        }
+        else {
+            System.out.println(name + " is" + " FAIL");
+        }
+    }
+}
