dfd8adc4e81a813b38e67821f25bbb7bc73e3a4b
utsav <utsav9591.10c@gmail.com>
2026-02-13 22:56:32 +0530

    change

--- src/Add_two_numbers.java
+++ src/Add_two_numbers.java
@@ -1,2 +1,10 @@
 public class Add_two_numbers {
+    public static void main(String[] args){
+        int n1=3;
+        int n2=4;
+
+        System.out.println(n1+n2);
+
+
+    }
 }
---
+++ src/greetings.java
@@ -0,0 +1,15 @@
+import javax.lang.model.element.Name;
+
+public class greetings {
+    public static void main(String[] args){
+        String  name="riya";
+        char Gender='F';
+        if(Gender=='M') {
+            System. out.println("HARE KRISHNA " + name + " prabhuji" );
+
+        }
+        else{
+            System. out.println("HARE KRISHNA " + name + " mataji" );
+        }
+    }
+}
